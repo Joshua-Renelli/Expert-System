1bb9f49eaafd69d934f8be05f5062235616454ee
Acul132 <jrenelli1@laurentian.ca>
2020-08-20 11:00:45 -0400

    Added production credentials

--- InferenceEngine/inference-engine.js
+++ InferenceEngine/inference-engine.js
@@ -1,6 +1,25 @@
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../expert-system-50e65-firebase-adminsdk-ot0ba-8e9b063ac7.json');
+var serviceAccount = null;
+
+if (process.env.NODE_ENV === 'production'){
+    console.log("PRODUCTION!!!")
+    serviceAccount = {
+        "type": "service_account",
+        "project_id": "expert-system-50e65",
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key,
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ot0ba%40expert-system-50e65.iam.gserviceaccount.com"
+      }
+} else {
+    serviceAccount = require('../expert-system-50e65-firebase-adminsdk-ot0ba-8e9b063ac7.json');
+}
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
